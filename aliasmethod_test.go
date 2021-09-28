4398e099f60cc7c37ccea566d0eccd1dfd161d1d
smartwalle <smartwalle@gmail.com>
2021-09-28 19:05:25 +0800

    修正 bug

--- aliasmethod_test.go
+++ aliasmethod_test.go
@@ -18,25 +18,25 @@ func (this *Christmas) Weight() int32 {
 func Test_AliasMethod(t *testing.T) {
 	var results = make(map[string]int)
 
+	var m = aliasmethod.New()
+
+	m.Add(&Christmas{name: "1", weight: 10})
+	m.Add(&Christmas{name: "2", weight: 10})
+	m.Add(&Christmas{name: "3", weight: 10})
+	m.Add(&Christmas{name: "4", weight: 10})
+	m.Add(&Christmas{name: "5", weight: 10})
+	m.Add(&Christmas{name: "6", weight: 10})
+	m.Add(&Christmas{name: "7", weight: 10})
+	m.Add(&Christmas{name: "8", weight: 10})
+	m.Add(&Christmas{name: "9", weight: 10})
+	m.Add(&Christmas{name: "10", weight: 110})
+
+	if err := m.Prepare(); err != nil {
+		t.Fatal(err)
+	}
+
 	for i := 0; i < 1000; i++ {
-		var m = aliasmethod.New()
-
-		m.Add(&Christmas{name: "1", weight: 10})
-		m.Add(&Christmas{name: "2", weight: 10})
-		m.Add(&Christmas{name: "3", weight: 10})
-		m.Add(&Christmas{name: "4", weight: 10})
-		m.Add(&Christmas{name: "5", weight: 10})
-		m.Add(&Christmas{name: "6", weight: 10})
-		m.Add(&Christmas{name: "7", weight: 10})
-		m.Add(&Christmas{name: "8", weight: 10})
-		m.Add(&Christmas{name: "9", weight: 10})
-		m.Add(&Christmas{name: "10", weight: 110})
-
-		if err := m.Prepare(); err != nil {
-			t.Fatal(err)
-		}
 		var p = m.NextItem()
-
 		var c = p.(*Christmas)
 		results[c.name] = results[c.name] + 1
 	}
